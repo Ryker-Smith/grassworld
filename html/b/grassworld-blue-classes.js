933f53ed4cc8d7d7f11d1df44cfc3bb46a7f893f
root <root@grassworld.fachtnaroe.net>
2020-05-28 18:04:01 +0100

    Post Submit

--- html/b/grassworld-blue-classes.js
+++ html/b/grassworld-blue-classes.js
@@ -3,52 +3,78 @@ var addnames=list_of_names[ grandom(list_of_names.length)-1 ]+ grandom(1000); //
 var blueSamurai; // variable name for our Samuari schplagen.
 
  class bluesamuraicharactersprite extends charactersprite {
-    tinteract(myId, otherId) {
-      if (thingmap.get(myId).o.Tgenus != thingmap.get(otherId).o.Tgenus){ 
-        // if we get myId of genus 25, this same genus is not equal to genus of otherId
-          // console.log("NOT ONE OF US");
-           // so keep myId of the same genus 'default' sprite.
-         thingmap.get(myId).sprite.heading='default';
+  constructor (options) {
+    this.Tid=options.Tid;
+  }
+    fighting() {
+      let d='fight';
+      let fightingspritesheet='bluesamurai.png';
+      let newbehavior={
+        d : {
+        "spritesheet" : fightingspritesheet,
+        "framecount" : "8",
+        "rowcount" : "1",
+        "w" : "1600",
+        "h" : "200",
+        "ticks" : "30",
+        "scale" : "0.2"
+        }
       }
-      else {
-        // if myId of current genus, make a fighting function for the sprite.
-            if (!thingmap.get(myId).sprite.isfighting) {
-              // change myId sprite to 'fight'.
-                thingmap.get(myId).sprite.heading='fight';
-                console.log("samuraii!!!");
-                // myId of current genus gets a random number between 1-100
-                thingmap.get(myId).sprite.mycoin=grandom(100);
-                // if myId gets a random number between 1-100, make fighting true.
-                thingmap.get(myId).sprite.isfighting=true;
-            }
-            else {
-              // if myId gets random number
-                if (thingmap.get(myId).sprite.mycoin==grandom(100)) {
-                   // charactersprite.pffft(otherId);
-                   // change myId sprite to 'fight' heading.
-                    thingmap.get(myId).sprite.heading='fight';
-                    thingmap.get(otherId).sprite.heading='fight';
-                    // otherId of same genus 25, is now being changed to genus 27 when attacked.
-                    thingmap.get(otherId).o.Tgenus=27;
-                    // calling a function from grassworld-classes "tput", making this schplágen post load function which loads its sprites after refresh.
-                    thingmap.get(otherId).o.tput(thisthingplf);
-                    // change myId genus of 25 to not being able to fight.
-                    thingmap.get(myId).sprite.isfighting=false;
-                    //
-                    thingmap.get(otherId).o.tget();
-  
-                }
-                else {
-                }
-            }
+      this.sprite.directions.set(d,JSON.parse(newbehavior)['fight']);
+      this.sprite.directions.get(d).spritesheet=new Image();
+      this.sprite.directions.get(d).spritesheet.src=imagewithfullpath(fightingspritesheet);
+      this.sprite.directions.get(d).ticks=Math.floor(thething.sprite.directions.get(d).ticks/world_speed_multiplier);
+      thingmap.get(this.Tid).sprite.frameIndex=0;
+      thingmap.get(this.Tid).sprite.tickCount=0;
+      thingmap.get(this.Tid).o.Gcanmove=false;
+      thingmap.get(this.Tid).o.ismoving=false;
+      thingmap.get(this.Tid).sprite.left_destination=thingmap.get(this.Tid).sprite.left;
+      thingmap.get(this.Tid).sprite.top_destination=thingmap.get(this.Tid).sprite.top;
+      thingmap.get(this.Tid).sprite.heading='fight';
+    }
+    peaceing() {
+      thingmap.get(this.Tid).sprite.heading='default';
+      thingmap.get(this.Tid).sprite.frameIndex=0;
+      thingmap.get(this.Tid).sprite.tickCount=0;
+      thingmap.get(this.Tid).o.Gcanmove=true;
+    }
+    interact(){
+    console.log("interacting blue");
+    if (thingmap.get(this.Tid).o.Ginteracts) {
+    switch (thingmap.get(this.Tid).o.Tgenus) {
+      case genus_blueSamurai : { 
+              let fightradius=51;
+              for (key of thingmap.keys()) {
+                  if (key == this.Tid) {
+                    continue;
+                  }
+                  let dist = distance({
+                    left: (thingmap.get(key).sprite.left + (thingmap.get(key).sprite.sprite_width / 2)),
+                    top: (thingmap.get(key).sprite.top + (thingmap.get(key).sprite.sprite_height / 2))
+                  }, {
+                    left: this.left + Math.floor(this.sprite_width/2),
+                    top: this.top + Math.floor(this.sprite_height/2)
+                  });
+                  if (dist < fightradius) {
+                    // do the fight with the first character in range
+                   // console.log("fight");
+                    this.o.Gcanmove=true;  
+                    this.o.fighting=true;
+                    this.o.Tstatus='somethingidonttknowwhat';
+                    break;
+                  }
+              }
+              
+        }
+        default : {
+           break;
+        }
       }
-    } 
-    
+    }
+  }
+
   } 
 
-var list_of_names=["Samurai ", "Samurai ","Samurai ","Samurai ","Samurai ","Samurai ","Samurai "]; // array with list of names
-var addnames=list_of_names[ grandom(list_of_names.length)-1 ]+ grandom(1000); // gets a new random name from the list of names.
-var blueSamurai; // variable name for our Samuari schplagen.
 
 function thisthingplf(r){
    for (var p in r.keys) {
@@ -92,7 +118,7 @@ function next_execute(r) {          // next executable to give schplagen propert
   thingmap.get(myTid).o.msaveLocation();
   //thingmap.get(Tid).o.delete();
   // gets the tkeypress from classes to be used as pressing keychars.
-/  thingmap.get(myTid).o.tkeypress = (function(keycode) { //keycode is used to get letters from numbers.
+   thingmap.get(myTid).o.tkeypress = (function(keycode) { //keycode is used to get letters from numbers.
       keychar=String.fromCharCode(keycode); 
       if (keychar=='S') {
           thingmap.get(thing_selected).o.sleepnow();  // if "S" key is pressed, gets the function from classes, passes it on and then is called to run
@@ -105,14 +131,13 @@ function next_execute(r) {          // next executable to give schplagen propert
       }
       console.log(" b2 wake")
     });
-  thingmap.get(myTid).o.tsavekeypress();  */
 
   
 //variable name given to my samurai interacting function.
- /* var samuraiinteractioncode=`
+  var samuraiinteractioncode=`
     if (thingmap.get(myId).o.Tgenus != thingmap.get(otherId).o.Tgenus){ 
     	// if we get myId of genus 25, this same genus is not equal to genus of otherId
-         console.log("NOT ONE OF US");
+//       console.log("NOT ONE OF US");
          // so keep myId of the same genus 'default' sprite.
        thingmap.get(myId).sprite.heading='default';
     }
@@ -167,7 +192,7 @@ function next_execute(r) {          // next executable to give schplagen propert
   
   console.log(samuraiinteractioncode);
   // making a tinteract function that has 3 variable names.
- // thingmap.get(myTid).o.tinteract = new Function('myId', 'otherId', samuraiinteractioncode);
+  thingmap.get(myTid).o.tinteract = new Function('myId', 'otherId', samuraiinteractioncode);
   // calling a tsaveinteract function which isn't being used right now.
   thingmap.get(myTid).o.tsaveinteract();
 
